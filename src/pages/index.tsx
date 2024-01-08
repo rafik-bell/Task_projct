0a00cca39ceac00f135e2ce83d0e7ea567656af1
Bellarbi Rafik <bellarbi.rafik7@gmail.com>
2024-01-07 23:13:04 -0800

    first commit test2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 
 import styles from '@/styles/Home.module.css'
-import { Box } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 
 
 
@@ -21,6 +21,7 @@ export default function Home() {
             Get started by editing&nbsp;
             <code className={styles.code}>src/pages/index.tsx</code>
           </p>
+          <Typography  className={styles.code}>ggggggggggg</Typography>
           <div>
             <a
               href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
